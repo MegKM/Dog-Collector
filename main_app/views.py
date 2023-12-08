74f235e0e74a2107158ff8a9202c93750b22e99f
MegKM <megankmithen@gmail.com>
2023-12-08 16:56:17 +1100

    added admin and model

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-
-dogs = [
-  {'name': 'Watson', 'breed': 'Bull Arab', 'description': 'cuddle monster', 'age': 4},
-  {'name': 'George', 'breed': 'Spoodle', 'description': 'fluffy', 'age': 6},
-]
+from .models import Dog
 
 def home(request):
     return render(request, 'home.html')
@@ -12,6 +8,13 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {
         'dogs': dogs
+    })
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', {
+        'dog': dog
     })
