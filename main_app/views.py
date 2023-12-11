2640c0e31bb5f331128eea560646e5859031f02c
MegKM <megankmithen@gmail.com>
2023-12-11 17:44:26 +1100

    added feeding functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Dog
+from .forms import FeedingForm
 
 def home(request):
     return render(request, 'home.html')
@@ -16,9 +18,19 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
+    feeding_form = FeedingForm()
     return render(request, 'dogs/detail.html', {
-        'dog': dog
+        'dog': dog,
+        'feeding_form': feeding_form
     })
+
+def add_feeding(request, dog_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        feeding = form.save(commit = False)
+        feeding.dog_id = dog_id
+        feeding.save()
+    return redirect('detail', dog_id = dog_id)
 
 class DogCreate(CreateView):
     model = Dog
