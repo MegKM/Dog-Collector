917f2866c820610ccb4a1ea98549122c805532cc
MegKM <megankmithen@gmail.com>
2023-12-11 16:14:18 +1100

    added CUD functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=200)
     age = models.IntegerField()
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
 
     def __str__(self):
         return f"{self.name}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 def home(request):
@@ -18,3 +19,15 @@ def dogs_detail(request, dog_id):
     return render(request, 'dogs/detail.html', {
         'dog': dog
     })
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields= '__all__'
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs'
